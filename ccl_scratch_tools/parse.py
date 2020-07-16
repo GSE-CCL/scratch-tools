5d8d56b4c9f0803e9b1ff1016ce28f622decb371
Joshua Archibald <jarchibald121@gmail.com>
2020-07-16 12:57:16 -0400

    Scratch is weird, part 2

--- ccl_scratch_tools/parse.py
+++ ccl_scratch_tools/parse.py
@@ -258,6 +258,11 @@ class Parser():
             for target in scratch_data["targets"]:
                 for block in target["blocks"]:
                     block = target["blocks"][block]
+
+                    # Handle weird cases
+                    if type(block) == list or "opcode" not in block:
+                        continue
+                    
                     for category in self.block_data:
                         if block["opcode"] in self.block_data[category]:
                             categories[category] += 1
